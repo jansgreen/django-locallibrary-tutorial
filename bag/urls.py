fccf6dee8beeccc719012eaf92460ab80d9082b6
Luis Pena <Luis Pena>
2020-08-12 18:54:39 -0400

    saving bag shoppin

--- bag/urls.py
+++ bag/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from . import views
+from . import views, mymovies
 
 urlpatterns = [
     path('', views.view_bag, name='bag'),
     path('<id_movie>', views.id_movie, name='id_movie'),
     path('<movie_title>', views.a_movie, name='a_movie'),
-    path('<id_movie>', views.push_bag, name='push_bag'),
+    path('add/<id_movie>', views.push_bag, name='push_bag'),
+    #path('', mymovies.my_movies, name='my_movies'),
+
 
 ]
 
-#r'^panel/person/(?P<person_id>[0-9]+
--- bag/views.py
+++ bag/views.py
@@ -8,27 +8,22 @@ from django.db.models import Q
 
 
 def view_bag(request):
-    """ A view that render """
-
-    search = request.GET.get("Search")
+    """A view show a user bag, all code come from code intitute, but they have some slight changes"""
     galeries = Movies.objects.all().order_by('title')
-    if search:
-        galeries = Movies.objects.filter(
-            Q(title__icontains = search) | Q(release_date__icontains = search)
-            ).distinct()
-            
-        if not galeries:
-            messages.add_message(request, messages.INFO, 'The movie '+search+' you looking far is not found!')
     paginator = Paginator(galeries, 6)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
         'page_obj': page_obj,
         }
-    return render(request, 'index.html', context)
+    return render(request, 'bag/bag.html', context)
+
+
 
 def push_bag(request, id_movie):
-    print(id_movie)
+    """A view show a user bag, all code come from code intitute, but they have some slight changes"""
+
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -39,8 +34,7 @@ def push_bag(request, id_movie):
         bag[id_movie] = quantity
     
     request.session['bag'] = bag
-    print(request.session['bag'])
-    return redirect(request,redirect_url)
+    return redirect(redirect_url)
 
 def id_movie(request, id_movie):
     galeries = Movies.objects.all().order_by('id')
@@ -63,4 +57,6 @@ def a_movie(request, movie_title):
           'galeries' : galeries 
         }
         return render(request, 'bag/Amovie.html', context)
+
+        
 
